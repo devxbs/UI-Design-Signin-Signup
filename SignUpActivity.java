e0d21453cdecae14eee959e168827ab239b0149e
devxbs <34465013+devxbs@users.noreply.github.com>
2018-06-24 14:47:57 +0700

    Update SignUpActivity.java

--- SignUpActivity.java
+++ SignUpActivity.java
@@ -22,7 +22,7 @@ public class SignUpActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.signup_activity);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
